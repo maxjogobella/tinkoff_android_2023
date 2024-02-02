3362c6069371abf2619d612f13a633ed239d4d7f
maxjogobella <maxsv_it@tuta.io>
2024-02-02 19:35:02 +0300

    add view binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,21 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.myapplication.client.ApiFactory
+import com.example.myapplication.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        ApiFactory.apiService.getTopMovie()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_DATA", it.listOfTopMovies.toString())
-            }, {
-                Log.d("TEST_DATA", it.message.toString())
-            })
+        setContentView(binding.root)
     }
 }
